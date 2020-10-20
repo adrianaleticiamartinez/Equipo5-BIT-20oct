a56e1c7cdb8ac26f4c4e2923d07fd409fa7f0f45
Articsteve <johnplutosmithsword@gmail.com>
2020-10-20 09:16:23 -0500

    spring app created

---
+++ src/main/java/com/bit/retobbva/RetoBbvaApplication.java
@@ -0,0 +1,13 @@
+package com.bit.retobbva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoBbvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoBbvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bit/retobbva/RetoBbvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.retobbva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoBbvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
